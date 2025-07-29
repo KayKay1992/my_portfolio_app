8d11649c0a0864fd65c060ea0148173ff821fc54
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-07-29 15:32:42 -0700

    Integrating navbar nav_item links and footer with social media links

--- portfolio-creative-tutorial/src/app/components/Header.tsx
+++ portfolio-creative-tutorial/src/app/components/Header.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { createClient } from '@/prismicio';
 import Link from 'next/link';
 import { PrismicNextLink } from '@prismicio/next';
+import NavBar from './Navbar';
 
 export default async function Header(){
     const client = createClient();
     const settings = await client.getSingle('settings');
     return (
         <header className='top-0 z-50 mx-auto max-w-7xl md:sticky md:top-4'>
-        <nav className="mb-0 pb-0">
+        {/* <nav className="mb-0 pb-0">
             <ul>
                 <li>
                     <Link href='/' aria-label='homepage'>
@@ -24,7 +25,8 @@ export default async function Header(){
                 ))}
                 
             </ul>
-        </nav>
+        </nav> */}
+        <NavBar settings={settings}/>
         </header>
     )
 }
